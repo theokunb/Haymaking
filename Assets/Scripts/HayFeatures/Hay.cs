df8a0979459a53440bc7f3ace47ceeb5b782f462
Алексей Макаров <33080157+theokunb@users.noreply.github.com>
2025-04-26 22:12:56 +0700

    added skin to grass

--- Assets/Scripts/HayFeatures/Hay.cs
+++ Assets/Scripts/HayFeatures/Hay.cs
@@ -1,36 +1,30 @@
-using System;
-using System.IO;
 using UnityEngine;
 
 public class Hay : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _spriteRenderer;
-    
-    private Sprite _normalHay;
-    private Sprite _cuttedHay;
+    [SerializeField] private Animator _animator;
 
     protected HayStatus _hayStatus;
     public HayStatus HayStatus => _hayStatus;
 
+
     private void Start()
     {
-        _normalHay = Resources.Load<Sprite>(Path.Combine("Hay"));
-        _cuttedHay = Resources.Load<Sprite>(Path.Combine("HayCutted"));
-
         _hayStatus = HayStatus.Normal;
-        _spriteRenderer.sprite = _normalHay;
         _spriteRenderer.color = new Color(1, 1, 1, 1);
     }
 
-    public void Cut()
+    public void Cut() 
     {
         _hayStatus = HayStatus.Cutted;
-        _spriteRenderer.sprite = _cuttedHay;
+        _animator.SetTrigger("Cut");
     }
 
     public void Grab()
     {
         _hayStatus = HayStatus.Grabbed;
         _spriteRenderer.color = new Color(0, 0, 0, 0);
+
     }
 }
