30355f54fce9a7133ddae06341cd9866510b09ff
Алексей Макаров <33080157+theokunb@users.noreply.github.com>
2025-04-27 09:02:42 +0700

    Task-42 added respawn grass

--- Assets/Scripts/HayFeatures/Hay.cs
+++ Assets/Scripts/HayFeatures/Hay.cs
@@ -1,7 +1,10 @@
+using System.Collections;
 using UnityEngine;
 
 public class Hay : MonoBehaviour
 {
+    [SerializeField] private float _minRespawnTime;
+    [SerializeField] private float _maxRespawnTime;
     [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private Animator _animator;
 
@@ -26,5 +29,22 @@ public class Hay : MonoBehaviour
         _hayStatus = HayStatus.Grabbed;
         _spriteRenderer.color = new Color(0, 0, 0, 0);
 
+        StartCoroutine(RespawnTask());
+    }
+
+    private IEnumerator RespawnTask()
+    {
+        var spawnTine = Random.Range(_minRespawnTime, _maxRespawnTime);
+        var elapsedTime = 0f;
+
+        while (elapsedTime < spawnTine)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        _hayStatus = HayStatus.Normal;
+        _spriteRenderer.color = new Color(1, 1, 1, 1);
+        _animator.SetTrigger("Rost");
     }
 }
