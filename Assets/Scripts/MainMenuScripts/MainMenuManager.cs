59c8b93be79120e11b947660ad2a8eab9df6d4ba
DimaHovrov <DimaHovrov@example.com>
2025-04-27 09:17:50 +0900

    Add main menu

---
+++ Assets/Scripts/MainMenuScripts/MainMenuManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private Image sceneLoadAnimation;
+    [SerializeField]
+    private float animSpeed;
+    
+    void Start()
+    {
+        StartCoroutine(StartGameAnimation());
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    public void StartGame()
+    {
+        print("test");
+        StartCoroutine(ClosingSceneAnimation());
+    }
+
+    IEnumerator StartGameAnimation()
+    {
+        var imageTransparent = sceneLoadAnimation.color.a;
+        while (imageTransparent > 0)
+        {
+            imageTransparent -= animSpeed * Time.deltaTime;
+            sceneLoadAnimation.color = new Color(sceneLoadAnimation.color.r, sceneLoadAnimation.color.g, sceneLoadAnimation.color.b, imageTransparent);
+            yield return null; // ∆дЄм следующего кадра
+        }
+    }
+
+    IEnumerator ClosingSceneAnimation()
+    {
+        var imageTransparent = sceneLoadAnimation.color.a;
+        while (imageTransparent < 1)
+        {
+            imageTransparent += animSpeed * Time.deltaTime;
+            print(imageTransparent);
+            sceneLoadAnimation.color = new Color(sceneLoadAnimation.color.r, sceneLoadAnimation.color.g, sceneLoadAnimation.color.b, imageTransparent);
+            yield return null; // ∆дЄм следующего кадра
+        }
+        SceneManager.LoadScene("SceneLeha");
+    }
+}
